cfa67fceee348f9534fa23ad7a3ea73609593f91
anlen.wang <anlen.wang@idreamsky.com>
2020-04-02 15:04:27 +0800

    antd统一样式

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
-//import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import './assets/css/base.less'
+import './assets/css/dsky-antd.less'
 import Router from './router'
-//import store from './store'
+import store from './store'
 import * as serviceWorker from './serviceWorker';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
 moment.locale('zh-cn');
 
 ReactDOM.render(
-    <ConfigProvider locale={zhCN}>
-        <Router />
-    </ConfigProvider>
+    <Provider store={store}>
+        <ConfigProvider locale={zhCN}>
+            <Router />
+        </ConfigProvider>
+    </Provider>
     , document.getElementById('root')
 );
 
---
+++ admin/src/store/action.js
@@ -0,0 +1,5 @@
+export function setPageTitle (data) {
+  return (dispatch, getState) => {
+    dispatch({ type: 'SET_PAGE_TITLE', data: data })
+  }
+}
